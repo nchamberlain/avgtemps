452788c3a0d1a651585cc01a8bed847845963740
Nelson <nelson.chamberlain7@gmail.com>
2024-11-03 09:09:45 -0800

    Building up basic components

--- avgs/models.py
+++ avgs/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+# Holds data imported from Weather Source for ONE location (city data station)
+# which has the temps for every day of every year between 1910's to 2020's
+class ImportTemps(models.Model):
+    station = models.CharField(max_length=32)
+    location = models.CharField(max_length=64)
+    tdate = models.DateField()
+    tmax = models.IntegerField(null=True)
+    tmin = models.IntegerField(null=True)
+    tobs = models.IntegerField(null=True)
+
+class Station(models.Model):
+    station = models.CharField(max_length=32)
+    description = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.description
+
+class AllTemps(models.Model):
+    tdate = models.DateField()
+    tmax = models.IntegerField(null=True)
+    tmin = models.IntegerField(null=True)
+    location = models.ForeignKey(Station, 
+                                 to_field="id",
+                                 on_delete=models.CASCADE,
+                                 null=True)
+    
+    def __str__(self):
+        return self.tdate.strftime('%Y-%m-%d: ') + str(self.location) + " High: " + str(self.tmax) + " Low: " + str(self.tmin)
--- basicsite/settings.py
+++ basicsite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "avgs.apps.AvgsConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
