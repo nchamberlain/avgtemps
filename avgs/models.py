cb75eb255e04954f8584876889d38988bf57e7a2
Nelson <nelson.chamberlain7@gmail.com>
2024-11-05 16:39:55 -0800

    Added verbose field names to ImportTemps

--- avgs/models.py
+++ avgs/models.py
@@ -3,12 +3,12 @@ from django.db import models
 # Holds data imported from Weather Source for ONE location (city data station)
 # which has the temps for every day of every year between 1910's to 2020's
 class ImportTemps(models.Model):
-    station = models.CharField(max_length=32)
-    location = models.CharField(max_length=64)
-    tdate = models.DateField()
-    tmax = models.IntegerField(null=True)
-    tmin = models.IntegerField(null=True)
-    tobs = models.IntegerField(null=True)
+    station = models.CharField("Unique ID for where temps were measured", max_length=32)
+    location = models.CharField("City, state, and Country", max_length=64)
+    tdate = models.DateField("Temperature's date")
+    tmax = models.IntegerField("Highest Temp for this day", null=True)
+    tmin = models.IntegerField("Lowest Temp for this day", null=True)
+    tobs = models.IntegerField("Ignore Time of Observation", null=True)
 
     def __str__(self):
         return self.location + " " + str(self.tdate) + " " + str(self.tmax) + " " + str(self.tmin)
