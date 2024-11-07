75672ff334aaa9ccda28d44b9f5e64ceb6d1429e
Nelson <nelson.chamberlain7@gmail.com>
2024-11-07 15:50:17 -0800

    Updated to retrieve Avgs, Max and Min temps

--- avgs/urls.py
+++ avgs/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index_view, name="index"),
+    path("daily/<int:id>", views.daily, name="daily"),
+    path('summary/<int:id>/', views.summary_view, name='summary'),
+    path('report/<int:id>/', views.report_view, name='report'),
 ]
--- avgs/views.py
+++ avgs/views.py
@@ -1,5 +1,40 @@
-from django.http import HttpResponse
+#modified to look like https://www.geeksforgeeks.org/how-to-add-url-parameters-to-django-template-url-tag/
+from django.shortcuts import get_object_or_404, render
+from django.utils import timezone
+from django.db.models import Avg, Max, Min
+from .models import ImportTemps
 
+#path="" on basicsite.urls.py calls avgs.urls.py which calls index_views.html to be
+#  returned to the browser. When the user selects option 1, 2, or 3, it hrefs to one
+#  of these 3 functions based on option selected. These in turn get the necessary
+#  info and render the display.html with this info but could just as easily call
+#  different pages to display the info
+def daily(request, id):
+    date = request.GET.get('date', 'Not Provided')
+    loc = "BERKELEY"  # this will become a param that is passed
+    currentdate = timezone.now() #this will become a param that is passed
+    mon = currentdate.month
+    day = currentdate.day
+    yr = currentdate.year
+    today_avg = ImportTemps.objects.filter(location__startswith=loc).filter(tdate__month=mon).filter(tdate__day=day).aggregate(Avg("tmax"), Avg('tmin'), Max('tmax'), Min('tmax'), Min('tmin'), Max('tmin'))
+    context = {'id': id, 
+               'loc':loc,
+               'mon':mon, 
+               'day':day, 
+               'yr': yr,
+               'today_avg': today_avg,
+               'type':'Daily Avgs'} #Type is shown in tab title
+    return render(request, "avgs/display_daily.html", context)
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the avgs index.")
+def summary_view(request, id):
+    month = request.GET.get('month', 'Not provided')
+    context = {'id': id, 'date': month, 'type': 'Select Date'}
+    return render(request, 'avgs/display.html', context)
+
+def report_view(request, id):
+    year = request.GET.get('year', 'Not provided')
+    context = {'id': id, 'date': year, 'type': 'Overall Avgs'}
+    return render(request, 'avgs/display.html', context)
+
+def index_view(request):
+  	return render(request, 'avgs/index.html')
